a18f1f30e22c1274c71bb13141de31dc572b61bc
Clément <82080806+clementcst@users.noreply.github.com>
2023-03-05 21:17:02 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,15 +4,15 @@ var closeFriends = document.querySelector(".close-friends");
 function settingMenuOpen(){
     
     if(settingMenu.id == "close"){
-        settingMenu.classList.toggle("setting-menu-height");
-        closeFriends.style.margin = "200px 0 0 0 ";
+        closeFriends.style.transitionDelay = "0s";
+        closeFriends.style.margin = "280px 0 0 0 ";
         settingMenu.id="open";
     }else{
-        settingMenu.classList.toggle("setting-menu-height");
+        closeFriends.style.transitionDelay = "0.14s";
         closeFriends.style.margin = "0 0 0 0 ";
         settingMenu.id="close";
     }
-
+    settingMenu.classList.toggle("setting-menu-height");
 }
 
 let openBtn = document.querySelector(".nav-open");
@@ -31,4 +31,13 @@ function closeNav(){
 
 openBtn.addEventListener("click", openNav);
 closeBtn.addEventListener("click",closeNav);
+
+var darkBtn = document.getElementById("dark-mode-btn")
+
+darkBtn.onclick = function (){
+    darkBtn.classList.toggle("dark-mode-on");
+    document.body.classList.toggle("dark-theme");
+}
+
+
 
