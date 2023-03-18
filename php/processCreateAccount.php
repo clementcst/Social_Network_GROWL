77331ea125317b3440a9fe447b4f062d1e468e72
JordanG2 <115002419+JordanG2@users.noreply.github.com>
2023-03-18 16:54:05 +0100

    Add files via upload

--- php/processCreateAccount.php
+++ php/processCreateAccount.php
@@ -6,14 +6,9 @@
     if(isset($_POST["submitRegistration"]))
     {
         $errorNumber = checkingForm("registration", $_POST);
-        java_log($errorNumber);
         if($errorNumber === 0)
         {
             $tabDataForm = $_POST;
-            // if(is_array(1) && 1 !== [] && array_sum(array()) === 0)
-            // {    
-                java_log("c bon");    
-                // $passwordHash = password_hash($tabDataForm["password"], PASSWORD_DEFAULT);
                 switch($tabDataForm["sex"]){
                     case "Man" :
                         $tabDataForm["sex"] = 1;
@@ -30,7 +25,6 @@
                 }
 
                 $passwordHash = password_hash($tabDataForm["password"], PASSWORD_DEFAULT);
-
                 $newUserTab = array("Username" => $tabDataForm["userName"],
                                     "Name" => $tabDataForm["name"],
                                     "Firstname" => $tabDataForm["firstName"],                                    
@@ -40,8 +34,7 @@
                                     "Country" => $tabDataForm["country"],
                                     "City" => $tabDataForm["city"],
                                     "PhoneNumber" => $tabDataForm["phoneNumber"],
-                                );
-                
+                                );                
                 $dbUser = db_newUser($newUserTab, $passwordHash);
                 if($dbUser != 1) {
                     switch ($dbUser) {
@@ -56,13 +49,13 @@
                     header('Location: ../createAccount.php', true, 301);
                 }
                 
-                // header('Location: ../index.php', true, 301);
+                header('Location: ../index.php', true, 301);
                 exit;
         }
         else
         {
             java_log("ici");
-            // header('Location: ../createAccount.php', true, 301);
+            header('Location: ../createAccount.php', true, 301);
             exit;
         }
     }
@@ -70,26 +63,7 @@
     {
         java_log("là");
         $_SESSION['errorMessageRegistration']['submitRegistration'] = "Veuillez envoyer le formulaire d'inscription.";
-        // header('Location: ../createAccount.php', true, 301);
+        header('Location: ../createAccount.php', true, 301);
         exit;
     }
-
-
-//     session_start();
-//    function verify_login(string $mail, string $password) {
-//       $filter = array( "Mail" => array("=", "'".$mail."'", "0"));
-//       $result_id = db_selectColumns("user", ['UserID'], $filter);
-//       if(count($result_id) != 1){
-//          java_log('erreur');
-//       } else {
-//          $filter = array( "UserID" => array("LIKE", "'".$result_id[0][0]."'", "0"));
-//          $result_pwd = db_selectColumns("password", ["EncrPwd"], $filter);
-//          if(password_verify($password ,$result_pwd[0][0])) {
-//             $SESSION['connnected'] = $result_id[0][0];
-//             java_log($SESSION['connnected']);
-//          }
-            
-//       }
-//    }
-
 ?>
