a52bd741cf24fcb8dfbf2e06875c1b54fb55c894
JordanG2 <115002419+JordanG2@users.noreply.github.com>
2023-03-25 23:40:39 +0100

    incorporation ajax dans la page conversation

--- js/conversations.js
+++ js/conversations.js
@@ -1,6 +1,8 @@
 function selectDiscussion(number_friend) {
     let actualselectedFriend = document.getElementsByClassName("selected_friends");
     let selectedFriend = document.getElementById(number_friend);
+	let nameSelectedFriend = document.getElementById('username_'+number_friend).innerHTML;
+    changeConversation(nameSelectedFriend);
     actualselectedFriend[0].classList.toggle("selected_friends");
     selectedFriend.classList.toggle("selected_friends");
 }
@@ -10,18 +12,26 @@ function sendMessage() {
 	var tableauMois = new Array('janvier','février','mars','mai','juillet','septembre','novembre','décembre');
     //On récupère le message qui est écrit dans le champ message
     let message = document.getElementById("actual_writen_message").value;
+	
     //On récupère la div conversation pour y ajouter la nouvelle bulle message
     let conversation = document.getElementById("conv");
+	
     //On crée la nouvelle bulle avec la bonne classe
     let new_bulle = document.createElement("div");
     new_bulle.className = "bulle my_bulle_message";
+	
     //Cette bulle doit contenir le contenu du message et les informations annexes avec les bonnes classes css
     let text_message = document.createElement("span");
     text_message.textContent = message;
     text_message.className = "text_message";
+	
     let info_message = document.createElement("span");
     info_message.className = "message_info date_message";
-	info_message.textContent = now.getHours() + ":" + now.getMinutes() + " - " + ('0'+now.getDate()).slice(-2) + " " + tableauMois[now.getMonth()];
+    var minutes = now.getMinutes()
+    if(minutes<10)
+    minutes = "0" + minutes;
+    date = now.getHours() + ":" + minutes + " - " + ('0'+now.getDate()).slice(-2) + " " + tableauMois[now.getMonth()];
+	info_message.textContent = date;
     //Pour la mise en forme, il faut une div vide qui centre à droite la bulle
     let empty_div = document.createElement("div");
     empty_div.className = "my-empty-conv"
@@ -36,4 +46,6 @@ function sendMessage() {
 
     conversation.prepend(mother_div);
     document.getElementById("actual_writen_message").value = "";
+    sendMessageIntoDB(message);
+    document.getElementById('current_speaking_date_last_message').innerHTML = date;
 }
