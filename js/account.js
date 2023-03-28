ba3fcddd781f6951ca95ed20bccd72bb17924648
Adamou02 <42783707+Adamou02@users.noreply.github.com>
2023-03-28 18:56:26 +0200

    Update account.js

--- js/account.js
+++ js/account.js
@@ -1,9 +1,13 @@
 function displayOnglet(name){
     if (name == "logIn"){
-        document.getElementById("form-registration").style.display="hidden";
-        document.getElementById("submit_Login").style.display="inline-block";
+        document.getElementById("log-in-account").classList.add("choosen-options");
+        document.getElementById("sign-up-account").classList.remove("choosen-options")
+        document.getElementById("form-registration").style.display="none";
+        document.getElementById("submit_Login").style.display="block";
     }else{
-        document.getElementById("form-registration").style.display="inline-block";
-        document.getElementById("submit_Login").style.display="hidden";
+        document.getElementById("sign-up-account").classList.add("choosen-options");
+        document.getElementById("log-in-account").classList.remove("choosen-options");
+        document.getElementById("form-registration").style.display="block";
+        document.getElementById("submit_Login").style.display="none";
     }
 }
