b3f063df7a9296552218ed28f72129dfea1b7095
Adamou02 <42783707+Adamou02@users.noreply.github.com>
2023-03-29 18:48:44 +0200

    Page inscription/connexion fini 99%
    Mettre le nom du site en vertical puis voir s'il y a besoin d'ajuster la min-height

--- createAccount.php
+++ createAccount.php
@@ -62,33 +62,27 @@
                     <div id="formulaire">
                         <div class="row row-space">
                             <div class="col-3">
-                                <div class="input-group input--style-2">
-                                    <input type="text" name="userName" id="userName" required
-                                        placeholder="Nom d'utilisateur" size="32"
-                                        value="<?php if(isset($tabCorrectInput["userName"])){echo $tabCorrectInput["userName"];}else{echo "";} ?>">
-                                    <p id="errorMessage-registration-userName" class="errorMessage">
-                                        <?php if(isset($tabErrorMessage["userName"])){echo $tabErrorMessage["userName"];}else{echo "";} ?>
-                                    </p>
-                                </div>
+                                <input type="text" name="userName" id="userName" required
+                                    placeholder="Nom d'utilisateur" size="32"
+                                    value="<?php if(isset($tabCorrectInput["userName"])){echo $tabCorrectInput["userName"];}else{echo "";} ?>">
+                                <p id="errorMessage-registration-userName" class="errorMessage">
+                                    <?php if(isset($tabErrorMessage["userName"])){echo $tabErrorMessage["userName"];}else{echo "";} ?>
+                                </p>
                             </div>
                             <div class="col-3">
-                                <div class="input-group input--style-2">
-                                    <input type="text" name="name" id="name" required placeholder="Nom" size="32"
-                                        value="<?php if(isset($tabCorrectInput["name"])){echo $tabCorrectInput["name"];}else{echo "";} ?>">
-                                    <p id="errorMessage-registration-name" class="errorMessage">
-                                        <?php if(isset($tabErrorMessage["name"])){echo $tabErrorMessage["name"];}else{echo "";} ?>
-                                    </p>
-                                </div>
+                                <input type="text" name="name" id="name" required placeholder="Nom" size="32"
+                                    value="<?php if(isset($tabCorrectInput["name"])){echo $tabCorrectInput["name"];}else{echo "";} ?>">
+                                <p id="errorMessage-registration-name" class="errorMessage">
+                                    <?php if(isset($tabErrorMessage["name"])){echo $tabErrorMessage["name"];}else{echo "";} ?>
+                                </p>
                             </div>
                             <div class="col-3">
-                                <div class="input-group input--style-2">
-                                    <input type="text" name="firstName" id="firstName" required placeholder="Prénom"
-                                        size="32"
-                                        value="<?php if(isset($tabCorrectInput["firstName"])){echo $tabCorrectInput["firstName"];}else{echo "";} ?>">
-                                    <p id="errorMessage-registration-firstName" class="errorMessage">
-                                        <?php if(isset($tabErrorMessage["firstName"])){echo $tabErrorMessage["firstName"];}else{echo "";} ?>
-                                    </p>
-                                </div>
+                                <input type="text" name="firstName" id="firstName" required placeholder="Prénom"
+                                    size="32"
+                                    value="<?php if(isset($tabCorrectInput["firstName"])){echo $tabCorrectInput["firstName"];}else{echo "";} ?>">
+                                <p id="errorMessage-registration-firstName" class="errorMessage">
+                                    <?php if(isset($tabErrorMessage["firstName"])){echo $tabErrorMessage["firstName"];}else{echo "";} ?>
+                                </p>
                             </div>
                         </div>
                         <div class="input_sexe flex">
@@ -114,91 +108,75 @@
                         </div>
                         <div class="row row-space birthday_tel">
                             <div class="col-2">
-                                <div class="input-group">
-                                    <input type="date" name="birthDate" id="birthDate" required
-                                        value="<?php if(isset($tabCorrectInput["birthDate"])){echo $tabCorrectInput["birthDate"];}else{echo "";} ?>">
-                                    <p id="errorMessage-registration-birthDate" class="errorMessage">
-                                        <?php if(isset($tabErrorMessage["birthDate"])){echo $tabErrorMessage["birthDate"];}else{echo "";} ?>
-                                    </p>
-                                </div>
+                                <input type="date" name="birthDate" id="birthDate" required
+                                    value="<?php if(isset($tabCorrectInput["birthDate"])){echo $tabCorrectInput["birthDate"];}else{echo "";} ?>">
+                                <p id="errorMessage-registration-birthDate" class="errorMessage">
+                                    <?php if(isset($tabErrorMessage["birthDate"])){echo $tabErrorMessage["birthDate"];}else{echo "";} ?>
+                                </p>
                             </div>
                             <div class="col-2">
-                                <div class="input-group">
-                                    <input type="tel" name="phoneNumber" id="phoneNumber" required placeholder="Téléphone"
-                                        value="<?php if(isset($tabCorrectInput["phoneNumber"])){echo $tabCorrectInput["phoneNumber"];}else{echo "";} ?>">
-                                    <p id="errorMessage-registration-phoneNumber" class="errorMessage">
-                                        <?php if(isset($tabErrorMessage["phoneNumber"])){echo $tabErrorMessage["phoneNumber"];}else{echo "";} ?>
-                                    </p>
-                                </div>
+                                <input type="tel" name="phoneNumber" id="phoneNumber" required placeholder="Téléphone"
+                                    value="<?php if(isset($tabCorrectInput["phoneNumber"])){echo $tabCorrectInput["phoneNumber"];}else{echo "";} ?>">
+                                <p id="errorMessage-registration-phoneNumber" class="errorMessage">
+                                    <?php if(isset($tabErrorMessage["phoneNumber"])){echo $tabErrorMessage["phoneNumber"];}else{echo "";} ?>
+                                </p>
                             </div>
                         </div>
                         <div class="row row-space">
                             <div class="col-2">
-                                <div class="input-group">
-                                    <input type="email" name="mail" id="mail" placeholder="Adresse mail" required size="25"
-                                        value="<?php if(isset($tabCorrectInput["mail"])){echo $tabCorrectInput["mail"];}else{echo "";} ?>">
-                                    <p id="errorMessage-registration-mail" class="errorMessage">
-                                        <?php if(isset($tabErrorMessage["mail"])){echo $tabErrorMessage["mail"];}else{echo "";} ?>
-                                    </p>
-                                </div>
+                                <input type="email" name="mail" id="mail" placeholder="Adresse mail" required size="25"
+                                    value="<?php if(isset($tabCorrectInput["mail"])){echo $tabCorrectInput["mail"];}else{echo "";} ?>">
+                                <p id="errorMessage-registration-mail" class="errorMessage">
+                                    <?php if(isset($tabErrorMessage["mail"])){echo $tabErrorMessage["mail"];}else{echo "";} ?>
+                                </p>
                             </div>
                             <div class="col-2">
-                                <div class="input-group">
-                                    <input type="email" name="confirmationMail" id="confirmationMail"
-                                        onpaste="return false;" required placeholder="Confirmation mail" size="25"
-                                        value="<?php if(isset($tabCorrectInput["confirmationMail"])){echo $tabCorrectInput["confirmationMail"];}else{echo "";} ?>">
-                                    <p id="errorMessage-registration-confirmationMail" class="errorMessage">
-                                        <?php if(isset($tabErrorMessage["confirmationMail"])){echo $tabErrorMessage["confirmationMail"];}else{echo "";} ?>
-                                    </p>
-                                </div>
+                                <input type="email" name="confirmationMail" id="confirmationMail"
+                                    onpaste="return false;" required placeholder="Confirmation mail" size="25"
+                                    value="<?php if(isset($tabCorrectInput["confirmationMail"])){echo $tabCorrectInput["confirmationMail"];}else{echo "";} ?>">
+                                <p id="errorMessage-registration-confirmationMail" class="errorMessage">
+                                    <?php if(isset($tabErrorMessage["confirmationMail"])){echo $tabErrorMessage["confirmationMail"];}else{echo "";} ?>
+                                </p>
                             </div>
                         </div>
                         <div class="row row-space">
                             <div class="col-2">
-                                <div class="input-group">
-                                    <input type="password" name="password" id="password" required
-                                        placeholder="Mot de passe">
-                                    <p id="errorMessage-registration-password" class="errorMessage">
-                                        <?php if(isset($tabErrorMessage["password"])){echo $tabErrorMessage["password"];}else{echo "";} ?>
-                                    </p>
-                                </div>
+                                <input type="password" name="password" id="password" required
+                                    placeholder="Mot de passe">
+                                <p id="errorMessage-registration-password" class="errorMessage">
+                                    <?php if(isset($tabErrorMessage["password"])){echo $tabErrorMessage["password"];}else{echo "";} ?>
+                                </p>
                             </div>
                             <div class="col-2">
-                                <div class="input-group">
-                                    <input type="password" name="confirmationPassword" id="confirmationPassword"
-                                        onpaste="return false;" required placeholder="Confirmation mot de passe">
-                                    <p id="errorMessage-registration-confirmationPassword" class="errorMessage">
-                                        <?php if(isset($tabErrorMessage["confirmationPassword"])){echo $tabErrorMessage["confirmationPassword"];}else{echo "";} ?>
-                                    </p>
-                                </div>
+                                <input type="password" name="confirmationPassword" id="confirmationPassword"
+                                    onpaste="return false;" required placeholder="Confirmation mot de passe">
+                                <p id="errorMessage-registration-confirmationPassword" class="errorMessage">
+                                    <?php if(isset($tabErrorMessage["confirmationPassword"])){echo $tabErrorMessage["confirmationPassword"];}else{echo "";} ?>
+                                </p>
                             </div>
                         </div>
                         <div class="row row-space">
                             <div class="col-2">
-                                <div class="input-group">
-                                    <input class="country" name="country" id="country" placeholder="Pays" required rows="3"
-                                        value="<?php if(isset($tabCorrectInput["country"])){echo $tabCorrectInput["country"];}else{echo "";} ?>">
-                                    <p id="errorMessage-registration-country" class="errorMessage">
-                                        <?php if(isset($tabErrorMessage["country"])){echo $tabErrorMessage["country"];}else{echo "";} ?>
-                                    </p>
-                                </div>
+                                <input class="country" name="country" id="country" placeholder="Pays" required rows="3"
+                                    value="<?php if(isset($tabCorrectInput["country"])){echo $tabCorrectInput["country"];}else{echo "";} ?>">
+                                <p id="errorMessage-registration-country" class="errorMessage">
+                                    <?php if(isset($tabErrorMessage["country"])){echo $tabErrorMessage["country"];}else{echo "";} ?>
+                                </p>
                             </div>
                             <div class="col-2">
-                                <div class="input-group">
-                                    <input type="text" name="city" id="city" placeholder="Ville" required
-                                        value="<?php if(isset($tabCorrectInput["city"])){echo $tabCorrectInput["city"];}else{echo "";} ?>">
-                                    <p id="errorMessage-registration-city" class="errorMessage">
-                                        <?php if(isset($tabErrorMessage["city"])){echo $tabErrorMessage["city"];}else{echo "";} ?>
-                                    </p>
-                                </div>
+                                <input type="text" name="city" id="city" placeholder="Ville" required
+                                    value="<?php if(isset($tabCorrectInput["city"])){echo $tabCorrectInput["city"];}else{echo "";} ?>">
+                                <p id="errorMessage-registration-city" class="errorMessage">
+                                    <?php if(isset($tabErrorMessage["city"])){echo $tabErrorMessage["city"];}else{echo "";} ?>
+                                </p>
                             </div>
                         </div>
                         <div class="row row-space">
                             <div class="col-2">
-                                <input type="submit" class="but_link" name="submitRegistration" value="Valider inscription">
+                                <input type="submit" class="btn" name="submitRegistration" value="Valider inscription">
                             </div>
                             <div class="col-2">
-                                <input type="reset" class="but_link" name="Reset" value="Annuler">
+                                <input type="reset" class="btn" name="Reset" value="Annuler">
                             </div>
                         </div>
                     </div>
@@ -208,33 +186,29 @@
                 <form id="submit_Login" action="./php/loginProcess.php" method="post" style="display: block;">
                     <div class="row row-space">
                         <div class="col-2">
-                            <div class="input-group">
-                                <input type="text" name="identifiant" id="identifiant" required
-                                    placeholder="Nom d'utilisateur/adresse mail" size="32"
-                                    value="<?php if(isset($_SESSION['conservedLogin'])){echo $_SESSION['conservedLogin'];unset($_SESSION['conservedLogin']);}else{echo "";} ?>">
-                                <p id="errorMessage-registration-log" class="errorMessage">
-                                    <?php if(isset($tab_errorMessage_log["log"])){echo $tab_errorMessage_log["log"];}else{echo "";} ?>
-                                </p>
-                            </div>
+                            <input type="text" name="identifiant" id="identifiant" required
+                                placeholder="Nom d'utilisateur/adresse mail" size="32"
+                                value="<?php if(isset($_SESSION['conservedLogin'])){echo $_SESSION['conservedLogin'];unset($_SESSION['conservedLogin']);}else{echo "";} ?>">
+                            <p id="errorMessage-registration-log" class="errorMessage">
+                                <?php if(isset($tab_errorMessage_log["log"])){echo $tab_errorMessage_log["log"];}else{echo "";} ?>
+                            </p>
                         </div>
                         <div class="col-2">
-                            <div class="input-group">
-                                <input type="password" name="password_log" id="password_log" required
-                                    placeholder="Mot de passe">
-                                <p id="errorMessage-registration-passwordLog" class="errorMessage">
-                                    <?php if(isset($tab_errorMessage_log["passwordLog"])){echo $tab_errorMessage_log["passwordLog"];}else{echo "";} ?>
-                                </p>
-                            </div>
+                            <input type="password" name="password_log" id="password_log" required
+                                placeholder="Mot de passe">
+                            <p id="errorMessage-registration-passwordLog" class="errorMessage">
+                                <?php if(isset($tab_errorMessage_log["passwordLog"])){echo $tab_errorMessage_log["passwordLog"];}else{echo "";} ?>
+                            </p>
                         </div>
                     </div>
                     <div class="row row-space">
-                            <div class="col-2">
-                                <input type="submit" class="but_link" name="submitLogin" value="Connexion">
-                            </div>
-                            <div class="col-2">
-                            <input type="reset" class="but_link" name="Reset" value="Annuler">
-                            </div>
+                        <div class="col-2">
+                            <input type="submit" class="btn" name="submitLogin" value="Connexion">
                         </div>
+                        <div class="col-2">
+                        <input type="reset" class="btn" name="Reset" value="Annuler">
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
