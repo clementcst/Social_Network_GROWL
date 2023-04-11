e8aafa2ac5295e962acdded1e63ddf700476efab
JordanG2 <115002419+JordanG2@users.noreply.github.com>
2023-04-11 18:48:16 +0200

    formulaire get conversation

--- js/listFriend.js
+++ js/listFriend.js
@@ -2,3 +2,7 @@ function submitFormProfilLink(form_no) {
     console.log("ouiuiuiui");
     document.getElementById("form-profil-link" + form_no).submit();
 }
+
+function submitFormConvLink(form_no) {
+    document.getElementById("form-conversation-link" + form_no).submit();
+}
