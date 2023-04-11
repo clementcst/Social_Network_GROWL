743ff450296444299cfeb53b70f4d31523b1ea95
Fab780 <104725129+Fab780@users.noreply.github.com>
2023-04-11 13:04:57 +0200

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,18 +1,22 @@
-let openProfile = document.querySelector("#profile");
-let profileMenu = document.querySelector(".profile-disclosed");
-let profileArrow = document.querySelector("#menu-arrow-profile");
+let openProfile = document.querySelectorAll("#profile");
+let profileMenu = document.querySelectorAll(".profile-disclosed");
+let profileArrow = document.querySelectorAll("#menu-arrow-profile");
 
-let openTheme = document.querySelector("#theme");
-let themeMenu = document.querySelector(".theme-disclosed");
-let themeArrow = document.querySelector("#menu-arrow-theme");
+let openTheme = document.querySelectorAll("#theme");
+let themeMenu = document.querySelectorAll(".theme-disclosed");
+let themeArrow = document.querySelectorAll("#menu-arrow-theme");
 
-let openPrivacy = document.querySelector("#privacy");
-let privacyMenu = document.querySelector(".privacy-disclosed");
-let privacyArrow = document.querySelector("#menu-arrow-privacy");
+let openPrivacy = document.querySelectorAll("#privacy");
+let privacyMenu = document.querySelectorAll(".privacy-disclosed");
+let privacyArrow = document.querySelectorAll("#menu-arrow-privacy");
 
-let openDelete = document.querySelector("#delete");
-let deleteMenu = document.querySelector(".delete-disclosed");
-let deleteArrow = document.querySelector("#menu-arrow-delete");
+let openDelete = document.querySelectorAll("#delete");
+let deleteMenu = document.querySelectorAll(".delete-disclosed");
+let deleteArrow = document.querySelectorAll("#menu-arrow-delete");
+
+let openPassword = document.querySelectorAll("#passwordChange");
+let passwordMenu = document.querySelectorAll(".password-disclosed");
+let passwordArrow = document.querySelectorAll("#menu-arrow-password");
 
 /*
     Menu opening functions
@@ -35,10 +39,11 @@ function openSettings(input, menu, arrow){
     Event listeners
 */
 
-openTheme.addEventListener('click', function(){openSettings(openTheme, themeMenu, themeArrow)});
-openProfile.addEventListener('click', function(){openSettings(openProfile, profileMenu, profileArrow)});
-openPrivacy.addEventListener('click', function(){openSettings(openPrivacy, privacyMenu, privacyArrow)});
-openDelete.addEventListener('click', function(){openSettings(openDelete, deleteMenu, deleteArrow)});
+openTheme[0].addEventListener('click', function(){openSettings(openTheme[0], themeMenu[0], themeArrow[0])});
+openProfile[0].addEventListener('click', function(){openSettings(openProfile[0], profileMenu[0], profileArrow[0])});
+openPrivacy[0].addEventListener('click', function(){openSettings(openPrivacy[0], privacyMenu[0], privacyArrow[0])});
+openDelete[0].addEventListener('click', function(){openSettings(openDelete[0], deleteMenu[0], deleteArrow[0])});
+openPassword[0].addEventListener('click', function(){openSettings(openPassword[0], passwordMenu[0], passwordArrow[0])});
 
 /*
     Confirm function
@@ -61,6 +66,30 @@ function delete_account(){
             });
         } else {
             swal("Your account was not deleted!", {
+                icon: "error",
+            });
+        }
+    });
+}
+
+function delete_friend(){
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, your friend will have to add you again!",
+        icon: "warning",
+        buttons: {
+            cancel: true,
+            confirm: "Delete",
+        },
+        dangerMode: true,
+    })
+    .then((willDelete) => {
+        if (willDelete) {
+            swal("Your friend was succesfully removed!", {
+                icon: "success",
+            });
+        } else {
+            swal("Your friend was not removed!", {
                 icon: "error",
             });
         }
