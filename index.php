dcd88845fa2ae1bf14535f4a48a9de3327f9d2c3
JordanG2 <115002419+JordanG2@users.noreply.github.com>
2023-04-17 15:16:23 +0200

    rollback v0.X

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <title>Home Social Network </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/index.css">
-    <script rel="stylesheet" src="js/script_ajaxRequest.js"></script>
 </head>
 
 <body>
