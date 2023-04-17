1e9287a15d8a1029a99f06cf3d18821bc81ad06e
Clément <82080806+clementcst@users.noreply.github.com>
2023-04-18 00:57:14 +0200

    Update conversations.js
    Clem

--- js/conversations.js
+++ js/conversations.js
@@ -7,6 +7,49 @@ function selectDiscussion(number_friend) {
     selectedFriend.classList.toggle("selected_friends");
 }
 
+function getfile(){
+    document.getElementById('hiddenfile').click();
+}
+
+function previewPicture(e){
+        
+    var divImages = document.getElementById("new-post-images");
+        Array.from(e.files).forEach(element => {
+
+            if (element) {
+                var oImg = document.createElement("img");
+                oImg.setAttribute('src', URL.createObjectURL(element));
+                oImg.setAttribute('accept', 'image/jpeg, image/jpg, image/png');
+                oImg.setAttribute('alt', 'na');
+                oImg.setAttribute('width', '500px');
+                oImg.setAttribute('class', 'post-input-images');
+
+                // Créer un bouton de suppression pour cette image
+                var deleteBtn = document.createElement("button");
+                //deleteBtn.setAttribute('width', '500px');
+                deleteBtn.innerHTML = "X";
+                deleteBtn.addEventListener("click", function() {
+                    divImages.removeChild(oImg);
+                    divImages.removeChild(deleteBtn);
+                });
+                
+                fetch(oImg.src) .then((res) => res.blob()) .then((blob) => {
+                    // Read the Blob as DataURL using the FileReader API
+                    const reader = new FileReader();
+                    reader.onloadend = () => {
+                        oImg.src = reader.result;
+                       
+                    };
+                    reader.readAsDataURL(blob);
+                });
+                
+                divImages.appendChild(oImg);
+                divImages.appendChild(deleteBtn);
+            }
+        })
+} 
+
+
 function sendMessage() {
 	let now = new Date();
 	var tableauMois = new Array('janvier','février','mars','mai','juillet','septembre','novembre','décembre');
