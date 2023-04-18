059eabbd589ba8264d41c9d92ddd2ea61b980b0a
Clément <82080806+clementcst@users.noreply.github.com>
2023-04-18 13:32:16 +0200

    Update aside.php

--- aside.php
+++ aside.php
@@ -1,23 +1,25 @@
+<link rel="stylesheet" href="css/aside.css">
 <aside class="left-content">
     <div class="nav-laterral">
         <div class="nav-close" id="nav-button">
             <ion-icon name="close"></ion-icon>
         </div>
         <div class="nav-links">
-            <a href="#"><div class="nav-link" >
+            
+            <a href=<?=INDEX?>><div class="nav-link" >
                 <ion-icon name="home"></ion-icon>Home
             </div></a>
             <a href=<?=PROFIL?>><div class="nav-link">
                 <ion-icon name="person"></ion-icon>Account
             </div></a>
-            <a href="#"><div class="nav-link">
+            <a href=<?=CONVERSATION?>><div class="nav-link">
                 <ion-icon name="paper-plane"></ion-icon>Messages
             </div></a>
-            <a href="#"><div class="nav-link">
+            <a href=<?=SETTINGS?>><div class="nav-link">
                 <ion-icon name="settings"></ion-icon>Settings
             </div></a>
             <hr>
-            <small>About us</small>
+            <a href=<?=ABOUTUS?>><small>About us</small></a>
         </div>
     </div>
     <nav class="lateral">
