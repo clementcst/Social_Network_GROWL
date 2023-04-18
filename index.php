e9b7f624cbcf36d887ff945e22c6b8df8829ca4e
Adamou02 <42783707+Adamou02@users.noreply.github.com>
2023-04-18 16:11:52 +0200

    Suppression de qques lignes oubliés

--- index.php
+++ index.php
@@ -88,53 +88,8 @@
                         </ion-icon>
                         <small><?= $nbComments ?></small>
                     </div>
-                    <!-- Comments -->
-                    <?php 
-                        for ($j=0; $j < $nbComments ; $j++) {
-                            $postCommentData = $postComments[$j]; 
-                            $postCommentData[2] = urldecode($postCommentData[2]);
-                            $CommentUserData = db_getUserData($postCommentData[3]);
-                    ?>
-                    <div class="comment-menu" id="close<?=$postData[0]?>">
-                        <div class="comments-list">
-                            <div class="user-profil comment-box">
-                                <img src="<?= $CommentUserData[9] ?>">
-                                <div class="">
-                                    <div class="comment-pseudo-text">
-                                        <a class="comment-pseudo"><?= $CommentUserData[0] ?></a>
-                                        <p class="comment-text"><?= $postCommentData[2] ?></p>
-                                    </div>
-                                    <div class="comment-reaction">
-                                        <p id="db_id_commentaire" class="comment-react comment-info" onclick="CreateInputTexte(this.id)">Répondre</p>
-                                        <!-- <p class="comment-react comment-info">Aimer</p> -->
-                                        <p class="comment-info"><?= $postCommentData[1] ?></p>
-                                    </div>
-                                </div>
-                                <!-- <div class="comment-number-like">
-                                    <p>
-                                        <ion-icon name="heart"></ion-icon><small>26</small>
-                                    </p>
-                                </div> -->
-
-                            </div>
-                            
-                            <!-- <div class="load-comments">
-                                <div class="comments-bar"></div>
-                                <span class="show-more-comments">Afficher les 4 réponses</span>
-                            </div> -->
-                            <div id="db_id_commentaire_answer_section" class="answers_div"> <!-- Generer cette id avec concatenation de bd_id et "_answer_section"-->
-                        </div>
-                        
-                            
-                        </div>
-                        <div class="send_menu_comment">
-                            <input id="actual_writen_message" type="text" class="form-control comment-input" placeholder="Write your comment"  onkeypress="if (event.keyCode == 13) sendComment()">
-                            <div class="send_icon_message" onclick="sendComment()">
-                                <ion-icon name="send"></ion-icon>
-                            </div>
-                        </div>
-                    </div>
-                    <?php } ?>
+                    <!-- Comments. Ils sont générés en ajax ici -->
+                    
                     <div>
                         <ion-icon name="share-social"></ion-icon>
                         <small><?= $postData[3] ?></small>
