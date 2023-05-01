93d83aec120632685cbcf254c633233d224ec786
LegJo <106883191+LegJo@users.noreply.github.com>
2023-05-01 12:45:04 +0200

    prep v0.9

--- php/constant.php
+++ php/constant.php
@@ -14,15 +14,16 @@
         define('SESSION','session.php');
         define('MISC','miscellanous.php');
         define('AJAX_R','ajaxRequest.php');
-        define('M_FORMS','manage_form.php');
-        define('ACCOUNT_PRO','processCreateAccount.php');
+        define('M_FORMS','manageForm.php');
+        define('ACCOUNT_PRO','accountProcess.php');
         define('LOGIN_PRO','loginProcess.php');
         define('SETTINGS_PRO','settingsProcess.php');
-        define('ABOUTUS','aboutUs.php');
+        define('FRIEND_PRO','friendProcess.php');
+        define('POST_PRO','postProcess.php');
 
     /** in ROOT */
         define('INDEX','index.php');
-        define('ACCOUNT','createAccount.php');
+        define('ACCOUNT','account.php');
         define('CONVERSATION','conversation.php');
         define('HEADER','header.php');
         define('LISTFRIEND','listFriend.php');
@@ -30,4 +31,5 @@
         define('FOOTER','footer.php');
         define('PROFIL','profil.php');
         define('SETTINGS','settings.php');
+        define('ABOUTUS','aboutUs.php');
 ?>
--- php/session.php
+++ php/session.php
@@ -4,6 +4,7 @@
        require_once("./required.php");
      else
        require_once("./php/required.php");
+   
 
     function s_isConnected(){
         return isset($_SESSION['connected']);
