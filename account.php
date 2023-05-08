401da53e398c1c6dbeb66ed7d9be64a67fea7581
JordanG2 <115002419+JordanG2@users.noreply.github.com>
2023-05-08 17:49:29 +0200

    Update account.php

--- account.php
+++ account.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login / Register GROWNL</title>
     <link rel="stylesheet" href="css/account.css">
+    <link rel="icon" type="image/ico" href="./images/growl_ico.ico">
 </head>
 
 <body>
