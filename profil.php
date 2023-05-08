73d6c0a41d9686981c0ba17f33aab33966bea868
LegJo <106883191+LegJo@users.noreply.github.com>
2023-05-08 17:27:33 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -4,8 +4,9 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Profile</title>
+        <title>Profil</title>
         <link rel="stylesheet" href="css/profil.css">
+        <link rel="icon" type="image/ico" href="./images/growl_ico.ico">
         <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
         
     </head>
