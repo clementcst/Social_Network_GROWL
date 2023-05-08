1246a90f2a80e26445f13eba1543e15093dc9c71
LegJo <106883191+LegJo@users.noreply.github.com>
2023-05-09 01:29:17 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -8,6 +8,7 @@
         <link rel="stylesheet" href="css/profil.css">
         <link rel="icon" type="image/ico" href="./images/growl_ico.ico">
         <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">   
     </head>
 
     <body>
