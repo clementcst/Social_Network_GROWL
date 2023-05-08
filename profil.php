5df5b1ac7582463858e4998208c8104c507de1ae
LegJo <106883191+LegJo@users.noreply.github.com>
2023-05-08 18:39:14 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -4,11 +4,10 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Profil</title>
+        <title>Profil | GROWL</title>
         <link rel="stylesheet" href="css/profil.css">
-        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
         <link rel="icon" type="image/ico" href="./images/growl_ico.ico">
-        
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>        
     </head>
 
     <body>
