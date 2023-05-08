10aa07dd73fdf49e990d5659774e28f34b37a965
LegJo <106883191+LegJo@users.noreply.github.com>
2023-05-08 17:25:48 +0200

    Update account.php

--- account.php
+++ account.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login / Register GROWNL</title>
     <link rel="stylesheet" href="css/account.css">
+    <link rel="icon" type="image/ico" href="./images/growl_ico.ico">
 </head>
 
 <body>
