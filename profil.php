1f1a8f90c4442c91a83299e5922af2e36d774eea
LegJo <106883191+LegJo@users.noreply.github.com>
2023-05-08 18:38:12 +0200

    PAS TOUT LE TEMPS "my post" dans profils quand aut

--- profil.php
+++ profil.php
@@ -4,10 +4,10 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Profil | GROWL</title>
+        <title>Profil</title>
         <link rel="stylesheet" href="css/profil.css">
-        <link rel="icon" type="image/ico" href="./images/growl_ico.ico">
         <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <link rel="icon" type="image/ico" href="./images/growl_ico.ico">
         
     </head>
 
@@ -52,7 +52,6 @@
                         </script><?php
                     break;
                     default:
-                        # code...
                         break;
                 }
             }
@@ -126,7 +125,7 @@
                 <div class="bottom-profil">
                     
                     <div class="container-btn-post">
-                        <btn id="box1" class="btn-active-profil">My Posts</btn>
+                        <btn id="box1" class="btn-active-profil"><?php if($vieweduser_id != $_SESSION['connected']) { echo "Posts of ".$vieweduserData[0]; } else { echo "My Posts"; } ?></btn>
                         <btn id="box2"  class="btn-unactive-profil">Liked Posts</btn>
                         <btn id="box3"  class="btn-unactive-profil">Shared Posts</btn>
                     </div>
@@ -173,7 +172,7 @@
                                                     <?php } 
                                                 } ?>
                                             </div>
-                                            <div class="post-reactions"  <?php if($postData[4] == 0) echo 'style="flex-direction:row;"'?>>
+                                            <div class="post-reactions">
                                                 <div>
                                                     <ion-icon name="heart" onclick="LikePost('<?= $postData[0] ?>', this, <?=$formCount?>)"
                                                         <?php if(count(db_selectColumns("liked_post", ["*"], ["UserID" => ["LIKE", "'".$_SESSION["connected"]."'", "1"],
@@ -253,7 +252,7 @@
                                                         <?php } 
                                                     } ?>
                                                 </div>
-                                                <div class="post-reactions"  <?php if($postData[4] == 0) echo 'style="flex-direction:row;"'?>>
+                                                <div class="post-reactions">
                                                     <div>
                                                         <ion-icon name="heart" onclick="LikePost('<?= $postData[0] ?>', this, <?=$formCount?>)"
                                                             <?php if(count(db_selectColumns("liked_post", ["*"], ["UserID" => ["LIKE", "'".$_SESSION["connected"]."'", "1"],
@@ -333,7 +332,7 @@
                                                         <?php } 
                                                     } ?>
                                                 </div>
-                                                <div class="post-reactions" <?php if($postData[4] == 0) echo 'style="flex-direction:row;"'?>>
+                                                <div class="post-reactions">
                                                     <div>
                                                         <ion-icon name="heart" onclick="LikePost('<?= $postData[0] ?>', this, <?=$formCount?>)"
                                                             <?php if(count(db_selectColumns("liked_post", ["*"], ["UserID" => ["LIKE", "'".$_SESSION["connected"]."'", "1"],
