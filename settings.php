add194bdb16d9ab5978b4faf62419dd8a0b1d2cc
LegJo <106883191+LegJo@users.noreply.github.com>
2023-05-08 17:28:00 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -4,9 +4,10 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Home Social Network </title>
+        <title>Settings | GROWL</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="css/settings.css">
+        <link rel="icon" type="image/ico" href="./images/growl_ico.ico">
     </head>
 
     <body>
